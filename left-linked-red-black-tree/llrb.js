30de09ae8a21fed69a3741d3f5f7a94f9edd841e
nv-gordeev <nv-gordeev@ya.ru>
2018-10-11 00:03:23 +0500

    added standard bst methods (height, isBalanced, isBST)

--- left-linked-red-black-tree/llrb.js
+++ left-linked-red-black-tree/llrb.js
@@ -22,6 +22,35 @@ class LLRBT {
         return node.color === RED
     }
 
+    height() {
+        return this._height(this.root);
+    }
+
+    isBalanced() {
+        return this._isBalanced(this.root)
+    }
+
+    isBST() {
+        return this._isBST(this.root, null, null)
+    }
+
+    _isBalanced(node) {
+        if (node === null) return true;
+        return this._height(node.left) - this._height(node.right) <= 1 && this._isBalanced(node.left) && this._isBalanced(node.right) 
+    }
+
+    _isBST(node, left, right ){
+        if (node === null) return true;
+        if (left !== null && left.key >= node.key) return false;
+        if (right !== null && right.key <= node.key) return false;
+        return this._isBST(node.left, left, node) && this._isBST(node.right, node, right)
+    }
+
+    _height(node) {
+        if (node === null) return -1;
+        return 1 + Math.max(this._height(node.left), this._height(node.right))
+    }
+
     _rotateLeft(node) {
         let x = node.right;
         node.right = x.left;
